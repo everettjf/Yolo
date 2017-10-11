6a75f1fa2e70a86ed7d70da41045cdf01b264342
everettjf <everettjf@live.com>
2017-10-12 02:45:11 +0800

    WeChatAppleTrace

---
+++ WeChatAppleTrace/WeChatAppleTraceDylib/WeChatAppleTraceDylib.h
@@ -0,0 +1,13 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  WeChatAppleTraceDylib.h
+//  WeChatAppleTraceDylib
+//
+//  Created by everettjf on 12/10/2017.
+//  Copyright (c) 2017 everettjf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
